4f2178b51c51f61a5fedca5bc07cb0f2f97fa5e9
Kevin Nwoke <kevinnwoke@yahoo.com>
2023-05-23 14:00:52 -0500

    Update YelpScraper.R

--- YelpScraper.R
+++ YelpScraper.R
@@ -1,4 +1,3 @@
-#install the necessary packages
 install.packages("rtools")
 install.packages("tidyverse")
 install.packages("rvest")
@@ -7,6 +6,8 @@ install.packages("rlist")
 install.packages("magrittr")
 install.packages("stringr")
 install.packages("tryCatchLog")
+install.packages("rlang")
+installed.packages("dplyr")
 
 # load the necessary packages
 library(tidyverse)
@@ -16,15 +17,13 @@ library(rlist)
 library(magrittr)
 library(stringr)
 
-#defining the url for the scraper
 url <- "https://www.yelp.com/biz/katzs-delicatessen-new-york?sort_by=date_asc"
+#download.file(url, destfile = "scrapedpage.html", quiet=TRUE)
 
-#read the HTML data into dataframe
 page <- read_html(url)
 
 df_final <- list()
 
-#identify the page navigation element on Yelp and convert this to a number
 pageNums <- page %>% 
   html_elements(xpath = "//div[@aria-label='Pagination navigation']") %>%
   html_text() %>% 
@@ -32,79 +31,88 @@ pageNums <- page %>%
   str_remove('of ') %>% 
   as.numeric()
 
-#incrementing the page navigation element
-pageSequence <- seq(from = 0, to = (pageNums * 10)-10, by=10)
+pageSequence <- seq(from = 9640, to = (pageNums * 10)-10, by=10)
 
 for(i in pageSequence) {
   
-  #convering the url into an iterable function
   url <- sprintf("https://www.yelp.com/biz/katzs-delicatessen-new-york?start=%d&sort_by=date_asc", i)
   
-  #convert HTML element into dataframe
   page <- read_html(url)
   
-  #scraping the necessary elements
-  # usernames <- page %>%
-  #   html_elements(xpath = "//div[starts-with(@class, ' user-passport')]") %>%
-  #   html_elements(xpath = ".//a[starts-with(@href, '/user_details')]") %>%
-  #   html_text()%>%
+  usernames <- page %>%
+    html_elements(xpath = "//div[starts-with(@class, ' user-passport')]") %>%
+    html_elements(xpath = ".//a[starts-with(@href, '/user_details')]") %>%
+    html_text() %>%
+    replace(!nzchar(usernames), NA)
 
-  locations <- page %>%
+ locations <- page %>%
     html_elements(xpath = "//div[starts-with(@class, ' user-passport')]") %>%
     html_elements(xpath = ".//span[@class=' css-qgunke']") %>%
     html_text() %>%
-    .[.!="Location"]
-  
+    .[.!="Location"]%>% 
+    replace(!nzchar(locations), NA)
+
   comments <- page %>%
-    html_elements(xpath = "//div[starts-with(@class, ' margin')]") %>%
+    html_elements(xpath = "//div[starts-with(@class, ' margin-b2')]") %>%
     html_elements(xpath = "(.//p[starts-with(@class, 'comment')])[1]") %>%
-    html_text()
+    html_text() %>%
+    replace(!nzchar(comments), NA)
   
   ratings <- page %>%
-    html_elements(xpath = "//ul[starts-with(@class, ' undefined')]") %>%
-    html_elements(xpath = ".//div[starts-with(@class, ' margin')]") %>%
-    html_elements(xpath = ".//div[starts-with(@class, ' arrange')]") %>%
-    html_elements(xpath = ".//span[starts-with(@class, ' display')]") %>%
+    html_elements(xpath = "//li[starts-with(@class, ' margin-b5')]") %>%
+    html_elements(xpath = ".//div[starts-with(@class, '  border')]") %>%
     html_elements(xpath = "(.//div[contains(@aria-label, 'star rating')])[1]") %>%
     html_attr("aria-label") %>%
     str_remove_all(" star rating") %>%
-    as.numeric()
+    as.numeric() %>%
+    replace(!nzchar(ratings), NA)
 
   the_dates <- page %>%
-    html_elements(xpath = "//ul[starts-with(@class, ' undefined')]") %>%
+    html_elements(xpath = "//li[starts-with(@class, ' margin-b5')]") %>%
     html_elements(xpath = ".//div[starts-with(@class, '  border')]") %>%
-    html_elements(xpath = ".//div[starts-with(@class, ' margin')]") %>%
     html_elements(xpath = "(.//span[@class = ' css-chan6m'])[1]") %>%
-    html_text()
+    html_text() %>%
+    replace(!nzchar(the_dates), NA)
   
   number_of_followers <- page %>%
     html_elements(xpath = "(//ul[contains(@class, 'list__09f24')])") %>%
     html_elements(xpath = ".//div[starts-with(@class, ' user-passport')]") %>%
     html_elements(xpath = "(.//span[@class = ' css-1fnccdf'])[1]") %>%
     html_text() %>%
-    as.numeric()
+    as.numeric() %>%
+    replace(!nzchar(number_of_followers), NA)
   
   number_of_reviews <- page %>%
     html_elements(xpath = "(//ul[contains(@class, 'list__09f24')])") %>%
     html_elements(xpath = ".//div[starts-with(@class, ' user-passport')]") %>%
     html_elements(xpath = "(.//span[@class = ' css-1fnccdf'])[2]") %>%
     html_text() %>%
-    as.numeric()
+    as.numeric() %>%
+    replace(!nzchar(number_of_reviews), NA)
 
-#putting the scrapped elements into a dataframe
-  df_new <- list (date = the_dates,
+  #appsloveworld.com/r/100/72/scraping-with-rvest-complete-with-nas-when-tag-is-not-present
+  #https://stackoverflow.com/questions/64867505/how-to-write-na-for-missing-results-in-rvest-if-there-was-no-content-in-node-wi
+  
+  df_new <- list(
+                  username = usernames,
+                  #helpful = helpful,
+                  #thanks = thanks,
+                  #lovethis = lovethis,
+                  date = the_dates,
                   location = locations,
                   rating = ratings,
                   numberoffollowers = number_of_followers,
                   numberofreviews = number_of_reviews,
-                  comment = comments)
+                  comment = comments
+                  )
   
   df_new_table <- as.data.frame(df_new)
   
   df_final <- rbindlist(list(df_final, df_new_table))
   
-  Sys.sleep(sample(c(45,60), 1))
+  Sys.sleep(sample(c(75,90), 1))
 }
+
 
 
   
