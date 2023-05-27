ff2f9628faf07a35fb844be2bdcf6a201448323e
Kevin Nwoke <kevinnwoke@yahoo.com>
2023-05-27 11:02:21 -0500

    Update YelpApp.py

--- YelpApp.py
+++ YelpApp.py
@@ -11,13 +11,11 @@ st.sidebar.title("Sentiment Analysis of Yelp Reviews")
 st.markdown("This application is a Streamlit dashboard to analyze the sentiment of Yelp reviews 🥪")
 st.sidebar.markdown("Streamlit Analysis 🥪")
 
-col_names = ["username", "date", "Month", "Day", "Year", "City NOT TRIM"," State/Foreign Country NOT TRIM", "Coordinates Old", "rating", "numberoffollowers", "numberofreviews", "City", "State", "City State", "Coordinates New", "LAT", "LON", "comment"]
-
 DATA_URL = ("https://raw.githubusercontent.com/knwoke/YelpWebScraper/main/KATZReviewFinalJul2005May2023v3.csv")
 
 @st.cache_data(persist=True)
 def load_data():
-    data = pd.read_csv(DATA_URL, names = col_names)
+    data = pd.read_csv(DATA_URL)
     data['date'] = pd.to_datetime(data['date'])
     return data
 
